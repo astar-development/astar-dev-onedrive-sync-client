3faac06c8c9ca394100ed22069a1a4427c14c225
Jason Barden <jason.barden1@outlook.com>
2026-03-16 01:26:22 +0000

    feat: all files back, ACLN2002 - on App.axaml ... again

---
+++ src/AStar.Dev.OneDrive.Sync.Client/Controls/AppStatusBar.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AStar.Dev.OneDrive.Sync.Client.Controls;
+
+public partial class AppStatusBar : UserControl
+{
+    public AppStatusBar()
+    {
+        InitializeComponent();
+    }
+}
