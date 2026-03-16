135bede6c17cba0c71dd6da2fec06808f3478932
Jason Barden <jason.barden1@outlook.com>
2026-03-16 00:54:26 +0000

    feat: add, but don't use the new views

---
+++ src/AStar.Dev.OneDrive.Sync.Client/Views/AccountsView.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace AStar.Dev.OneDrive.Sync.Client.Views;
+
+public partial class AccountsView : UserControl
+{
+    public AccountsView() => InitializeComponent();
+}
